b1f203a9cb0142a8dc3168d005998977a8f07e96
KRL1991 <kimx4706@edu.easj.dk>
2020-12-02 11:08:31 +0100

    First Commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,125 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class Controller {
+
+    @FXML
+    private AnchorPane background;
+
+    @FXML
+    private Button saveButton;
+
+    @FXML
+    private TextField charName;
+
+    @FXML
+    private TextField strengthTextfield;
+
+    @FXML
+    private TextField dexterityTextfield;
+
+    @FXML
+    private TextField intelligenceTextField;
+
+    @FXML
+    private TextField constitutionTextfField;
+
+    @FXML
+    private TextField wisdomeTextField;
+
+    @FXML
+    private TextField charismaTextField;
+
+    @FXML
+    private TextField classAndLevelTextField;
+
+    @FXML
+    private TextField raceTextField;
+
+    @FXML
+    private TextField proficiencyTextField;
+
+    @FXML
+    private TextField savingStrenghtTextField;
+
+    @FXML
+    private TextField savingDexterityTextField;
+
+    @FXML
+    private TextField savingConstitutionTextField;
+
+    @FXML
+    private TextField savingIntelligenceTextField;
+
+    @FXML
+    private TextField savingWisdomeTextField;
+
+    @FXML
+    private TextField savingCharismaTextField;
+
+    @FXML
+    private TextField armorClassTextField;
+
+    @FXML
+    private TextField initiativeTextField;
+
+    @FXML
+    private TextField speedTextField;
+
+    @FXML
+    private TextField currentHitPointsTextField;
+
+    @FXML
+    private TextField hitDiceTextField;
+
+    @FXML
+    private Button loadButton;
+
+    @FXML
+    private TextField weapon1TextField;
+
+    @FXML
+    private TextField weapon2TextField;
+
+    @FXML
+    private TextField weapon3TextField;
+
+    @FXML
+    private Button saveButtonInv;
+
+    @FXML
+    private Button loadButtonInv;
+
+    @FXML
+    private TextArea inventoryTextArea;
+
+    @FXML
+    private TextArea wealthTextArea;
+
+    @FXML
+    void loadCharSheet(ActionEvent event) {
+
+    }
+
+    @FXML
+    void loadInventory(ActionEvent event) {
+
+    }
+
+    @FXML
+    void saveCharSheet(ActionEvent event) {
+
+    }
+
+    @FXML
+    void saveInventory(ActionEvent event) {
+
+    }
+
+}
