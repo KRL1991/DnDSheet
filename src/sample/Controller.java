9e755467832d000247864543066bdcfd9404863c
KRL1991 <kimx4706@edu.easj.dk>
2020-12-03 09:50:32 +0100

    Finnished Adding save and load function - it is not working yet, since it gives everything af value of null when loaded

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -7,6 +7,7 @@ import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class Controller {
@@ -105,7 +106,42 @@ public class Controller {
     private TextArea wealthTextArea;
 
     @FXML
-    void loadCharSheet(ActionEvent event) {
+    void loadCharSheet(ActionEvent event) throws FileNotFoundException {
+
+        FileHandling fileHandling = new FileHandling();
+        GameData gameData;
+        gameData = fileHandling.load();
+
+        charName.setText(gameData.charName);
+        strengthTextfield.setText(gameData.strengthTextfield);
+        dexterityTextfield.setText(gameData.dexterityTextfield);
+        intelligenceTextField.setText(gameData.intelligenceTextField);
+        constitutionTextfField.setText(gameData.constitutionTextfField);
+        wisdomeTextField.setText(gameData.wisdomeTextField);
+        charismaTextField.setText(gameData.charismaTextField);
+        classAndLevelTextField.setText(gameData.classAndLevelTextField);
+        raceTextField.setText(gameData.raceTextField);
+        proficiencyTextField.setText(gameData.proficiencyTextField);
+        savingStrenghtTextField.setText(gameData.savingStrenghtTextField);
+        savingDexterityTextField.setText(gameData.savingDexterityTextField);
+        savingIntelligenceTextField.setText(gameData.savingIntelligenceTextField);
+        savingConstitutionTextField.setText(gameData.savingConstitutionTextField);
+        savingWisdomeTextField.setText(gameData.savingWisdomeTextField);
+        savingCharismaTextField.setText(gameData.savingCharismaTextField);
+        armorClassTextField.setText(gameData.armorClassTextField);
+        initiativeTextField.setText(gameData.initiativeTextField);
+        speedTextField.setText(gameData.speedTextField);
+        currentHitPointsTextField.setText(gameData.currentHitPointsTextField);
+        hitDiceTextField.setText(gameData.hitDiceTextField);
+        weapon1TextField.setText(gameData.weapon1TextField);
+        weapon2TextField.setText(gameData.weapon2TextField);
+        weapon3TextField.setText(gameData.weapon3TextField);
+        inventoryTextArea.setText(gameData.inventoryTextArea);
+        wealthTextArea.setText(gameData.wealthTextArea);
+
+
+
+
 
     }
 
@@ -122,24 +158,28 @@ public class Controller {
         gameData.strengthTextfield = strengthTextfield.getText();
         gameData.dexterityTextfield = dexterityTextfield.getText();
         gameData.intelligenceTextField = intelligenceTextField.getText();
-        gameData.charName = charName.getText();
-        gameData.charName = charName.getText();
-        gameData.charName = charName.getText();
-        gameData.charName = charName.getText();
-        gameData.charName = charName.getText();
-        gameData.charName = charName.getText();
-        gameData.charName = charName.getText();
-        gameData.charName = charName.getText();
-        gameData.charName = charName.getText();
-        gameData.charName = charName.getText();
-        gameData.charName = charName.getText();
-        gameData.charName = charName.getText();
-        gameData.charName = charName.getText();
-        gameData.charName = charName.getText();
-        gameData.charName = charName.getText();
-        gameData.charName = charName.getText();
-        gameData.charName = charName.getText();
-        gameData.charName = charName.getText();
+        gameData.constitutionTextfField = constitutionTextfField.getText();
+        gameData.wisdomeTextField = wisdomeTextField.getText();
+        gameData.charismaTextField = charismaTextField.getText();
+        gameData.classAndLevelTextField = classAndLevelTextField.getText();
+        gameData.raceTextField = raceTextField.getText();
+        gameData.proficiencyTextField = proficiencyTextField.getText();
+        gameData.savingStrenghtTextField = savingStrenghtTextField.getText();
+        gameData.savingDexterityTextField = savingDexterityTextField.getText();
+        gameData.savingIntelligenceTextField = savingIntelligenceTextField.getText();
+        gameData.savingConstitutionTextField = savingConstitutionTextField.getText();
+        gameData.savingWisdomeTextField = savingWisdomeTextField.getText();
+        gameData.savingCharismaTextField= savingCharismaTextField.getText();
+        gameData.armorClassTextField = armorClassTextField.getText();
+        gameData.initiativeTextField = initiativeTextField.getText();
+        gameData.speedTextField = speedTextField.getText();
+        gameData.currentHitPointsTextField = currentHitPointsTextField.getText();
+        gameData.hitDiceTextField = hitDiceTextField.getText();
+        gameData.weapon1TextField = weapon1TextField.getText();
+        gameData.weapon2TextField = weapon2TextField.getText();
+        gameData.weapon3TextField = weapon3TextField.getText();
+        gameData.inventoryTextArea = inventoryTextArea.getText();
+        gameData.wealthTextArea = wealthTextArea.getText();
 
 
         FileHandling filehandling = new FileHandling();
@@ -148,7 +188,15 @@ public class Controller {
     }
 
     @FXML
-    void saveInventory(ActionEvent event) {
+    void saveInventory(ActionEvent event) throws IOException {
+
+        GameData gameData = new GameData();
+        gameData.inventoryTextArea = inventoryTextArea.getText();
+        gameData.wealthTextArea = wealthTextArea.getText();
+
+        FileHandling fileHandling = new FileHandling();
+        fileHandling.save(gameData);
+
 
     }
 
